100f2d0649d21f3a1548175aa7cfe876e4850e02
Nayan993 <nayanmishra690@gmail.com>
2026-01-17 15:24:50 +0530

    Added infinte city logic & fog effect

---
+++ Deivesense1/Assets/Scripts/EndlessCity.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class EndlessCity : MonoBehaviour
+{
+ [SerializeField] Transform playerCarTransform;
+ [SerializeField] Transform otherCityTransform;
+ [SerializeField] float halfLength;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(playerCarTransform.position.z > transform.position.z + halfLength + 10f)
+        {
+            transform.position = new Vector3(0,0,otherCityTransform.position.z + halfLength * 2);
+        }
+    }
+}
