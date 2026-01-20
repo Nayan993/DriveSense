a29fb78c9f488082097fc0f0d013f4aeada4e4bc
Nayan993 <nayanmishra690@gmail.com>
2026-01-20 17:55:56 +0530

    Added Garage System Part 1 - cars selcetion method in garage

--- Deivesense1/Assets/Scripts/CarSelection.cs
+++ Deivesense1/Assets/Scripts/CarSelection.cs
@@ -7,13 +7,24 @@ public class CarSelection : MonoBehaviour
 
     void Start()
     {
+        ShowCar(currentCarIndex);
+    }
+    public void NextCar()
+    {
+    currentCarIndex = (currentCarIndex + 1) % cars.Length;
+    ShowCar(currentCarIndex);
+    }
+    public void PreviousCar()
+    {   
+    currentCarIndex = (currentCarIndex - 1 + cars.Length) % cars.Length;
+    ShowCar(currentCarIndex);
+    }
+
+    void ShowCar(int index)
+    {
         for(int i = 0; i < cars.Length; i++)
         {
-            cars[i].SetActive(i == currentCarIndex);
+            cars[i].SetActive(i == index);
         }
-    }
-    void Update()
-    {
-        
     }
 }
