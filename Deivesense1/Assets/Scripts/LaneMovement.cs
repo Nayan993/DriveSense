72361aa28593f9d60cc94cb7269944c98e8a4387
Anuj Sahu <anujsahu8847@gmail.com>
2026-01-22 18:24:52 +0530

    Documented Lane Movement Script

--- Deivesense1/Assets/Scripts/LaneMovement.cs
+++ Deivesense1/Assets/Scripts/LaneMovement.cs
@@ -2,24 +2,30 @@ using UnityEngine;
 
 public class LaneMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   [SerializeField] Transform playerCarTransform;
-    [SerializeField] float offSet = -5;
+    [SerializeField] Transform playerCarTransform; // Player car reference
+    [SerializeField] float offSet = -5; // Offset from car position
+
     void Start()
     {
-        
+        // Not used
     }
-     public void SetTransform(Transform transform)
+
+    public void SetTransform(Transform transform)
     {
+        // Assign player car transform
         playerCarTransform = transform;
     }
+
     // Update is called once per frame
     void LateUpdate()
     {
+        // Skip if car not assigned
         if(playerCarTransform == null)
         {
             return;
         }
+
+        // Follow car on Z axis
         Vector3 cameraPos = transform.position;
         cameraPos.z = playerCarTransform.position.z + offSet;
         transform.position = cameraPos;
