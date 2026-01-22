efabf2c73f345a66b382376ce799a0aefd6446b9
Nayan993 <nayanmishra690@gmail.com>
2026-01-22 18:05:19 +0530

    Documented Car Selection Script

--- Deivesense1/Assets/Scripts/CarSelection.cs
+++ Deivesense1/Assets/Scripts/CarSelection.cs
@@ -1,35 +1,43 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class CarSelection : MonoBehaviour
 {
-    [SerializeField] GameObject[] cars;
-    int currentCarIndex = 0;
+    [SerializeField] GameObject[] cars; // All selectable cars
+    int currentCarIndex = 0; // Currently selected car
 
     void Start()
     {
-        Time.timeScale = 1f;
+        Time.timeScale = 1f; // Ensure game runs normally
         ShowCar(currentCarIndex);
     }
+
     public void NextCar()
     {
-    currentCarIndex = (currentCarIndex + 1) % cars.Length;
-    ShowCar(currentCarIndex);
+        // Move to next car
+        currentCarIndex = (currentCarIndex + 1) % cars.Length;
+        ShowCar(currentCarIndex);
     }
+
     public void PreviousCar()
     {   
-    currentCarIndex = (currentCarIndex - 1 + cars.Length) % cars.Length;
-    ShowCar(currentCarIndex);
+        // Move to previous car
+        currentCarIndex = (currentCarIndex - 1 + cars.Length) % cars.Length;
+        ShowCar(currentCarIndex);
     }
 
     void ShowCar(int index)
     {
+        // Activate only the selected car
         for(int i = 0; i < cars.Length; i++)
         {
             cars[i].SetActive(i == index);
         }
     }
+
     public void SelectCar()
     {
+        // Save selected car index
         PlayerPrefs.SetInt("CarIndexValue", currentCarIndex);
     }
 }
