26d355d1d65664037e215592ec2fce143358c800
Anuj Sahu <anujsahu8847@gmail.com>
2026-01-22 18:21:59 +0530

    Documented Garage Music Script

--- Deivesense1/Assets/Scripts/GarageMusicController.cs
+++ Deivesense1/Assets/Scripts/GarageMusicController.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class GarageMusicController : MonoBehaviour
 {
-    private AudioSource musicSource;
+    private AudioSource musicSource; // Garage music source
 
     void Awake()
     {
+        // Get attached AudioSource
         musicSource = GetComponent<AudioSource>();
     }
 
     public void StopGarageMusic()
     {
+        // Stop music if it's playing
         if (musicSource != null && musicSource.isPlaying)
             musicSource.Stop();
     }
