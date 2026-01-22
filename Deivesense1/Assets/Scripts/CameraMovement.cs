48bcc2cbf5969f8e55d3911e64e350daa75d4bec
Nayan993 <nayanmishra690@gmail.com>
2026-01-22 18:04:12 +0530

    Documented Camera Movement script

--- Deivesense1/Assets/Scripts/CameraMovement.cs
+++ Deivesense1/Assets/Scripts/CameraMovement.cs
@@ -2,19 +2,24 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField] Transform playerCarTransform;
-    [SerializeField] float offSet = -5;
+    [SerializeField] Transform playerCarTransform; // Reference to the player car
+    [SerializeField] float offSet = -5; // Distance behind the car
+
     void Start()
     {
-        
+        // Intentionally left empty
     }
+
     public void SetTransform(Transform transform)
     {
+        // Assign the player car transform at runtime
         playerCarTransform = transform;
     }
+
     // Update is called once per frame
     void LateUpdate()
     {
+        // Keep camera aligned with car on Z axis
         Vector3 cameraPos = transform.position;
         cameraPos.z = playerCarTransform.position.z + offSet;
         transform.position = cameraPos;
